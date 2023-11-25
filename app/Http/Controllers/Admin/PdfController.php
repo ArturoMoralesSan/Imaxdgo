7fb02f38aaaa33333b3755bca8607931c07b80f8
ArturoMoralesDev <ramon.morales41@gmail.com>
2023-11-25 17:00:11 -0600

    Ajustar por orden DESC lo servicios.

--- app/Http/Controllers/Admin/PdfController.php
+++ app/Http/Controllers/Admin/PdfController.php
@@ -16,11 +16,9 @@ class PdfController extends Controller
     
     public function pdf($id)
     {
-        if (!Auth::user()->isSuperAdmin()) {
-            $id = Auth::user()->branch_id;
-        }
         $dateNow    = Carbon::now();
         $dateFormat = $dateNow->format('Y-m-d');
+
         $start_date = \Request('start_date') != null ? \Request('start_date') : $dateNow->subDays(5)->format('Y-m-d');
         $end_date   = \Request('end_date') != null ? \Request('end_date') : $dateFormat ;
 
@@ -28,10 +26,21 @@ class PdfController extends Controller
         $services = Service::whereBetween('date', [$start_date, $end_date])->orderBy('date')->where('branch_id', $id)->get();
         $expenses = Expense::whereBetween('date', [$start_date, $end_date])->orderBy('date')->where('branch_id', $id)->with('type_expense')->get();
         
+        
+        if (!Auth::user()->isSuperAdmin()) {
+            $start_date = \Request('start_date') != null ? \Request('start_date') : $dateFormat;
+            $end_date   = \Request('end_date') != null ? \Request('end_date') : $dateFormat ;
+
+            $id = Auth::user()->branch_id;
+        }
+
         $start_date = Carbon::createFromFormat('Y-m-d', $start_date)->format('d/m/Y');
         $end_date   = Carbon::createFromFormat('Y-m-d', $end_date)->format('d/m/Y');
 
+        
+        
         $pdf = PDF::loadView('admin.pdf.index', compact('services', 'expenses', 'start_date', 'end_date'));
+
         return $pdf->download('report.pdf');   
     }
 
